ad0cda5f53e00b0103b67e2829d5d238261f5a56
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 18:09:04 +0530

    Update Beam.jsx

--- src/components/Beam.jsx
+++ src/components/Beam.jsx
@@ -5,20 +5,18 @@ import googleIcon from "../assets/goog.svg";
 export function BackgroundBeamsWithCollisionDemo() {
   return (
     <BackgroundBeamsWithCollision>
-      <div className="flex flex-col items-center justify-center h-screen text-center overflow-hidden relative z-10">
-        {/* Heading with white text */}
+      <div className="flex flex-col items-center justify-center h-screen text-center overflow-hidden">
+        {/* Heading with white text only */}
         <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center font-sans tracking-tight text-white">
           What&apos;s cooler than{" "}
           <img src={googleIcon} alt="Google" className="inline-block" />{" "}
           Calendar?{" "}
           <br />
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-            {/* White text for Eventify */}
-            <div className="absolute left-0 top-[1px] text-white [text-shadow:0_0_rgba(0,0,0,0.1)]">
+            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)] dark:text-pink-500">
               <span className="">Eventify.</span>
             </div>
-            {/* White text for Eventify */}
-            <div className="relative text-white">
+            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
               <span className="">Eventify.</span>
             </div>
           </div>
