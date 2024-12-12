298be820a5103152f26e9aaba8beefbbe19519db
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-13 00:32:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,9 @@ function App() {
       <div id="beam">
       <BackgroundBeamsWithCollisionDemo />
       </div>
-
+        <div>
         <Contact/> 
-      
+      </div>
             
 
     </div>
