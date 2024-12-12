9fc5357a0a6954d3eb6478913332cb84d98b5f57
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 15:45:52 +0530

    Update LampDemo.jsx

--- src/components/LampDemo.jsx
+++ src/components/LampDemo.jsx
@@ -14,7 +14,7 @@ export function LampDemo() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl dark:text-gray-300"
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         Features
       </motion.h1>
