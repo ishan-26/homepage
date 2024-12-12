7c921bc97cf192e7e5fe47b1059a96e741d73c7b
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 23:34:19 +0530

    Update container-scroll.jsx

--- src/components/ui/container-scroll.jsx
+++ src/components/ui/container-scroll.jsx
@@ -69,19 +69,19 @@ export const Card = ({
   scale,
   children
 }) => {
-  return (
-    <motion.div
-      style={{
-        rotateX: rotate,
-        scale,
-        boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
-      }}
-      className="max-w-5xl mx-auto w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
-    >
-      <div className="w-full overflow-hidden md:rounded-2xl md:p-4 hidden sm:block">
-        {children}
-      </div>
-    </motion.div>
+  <motion.div
+  style={{
+    rotateX: rotate,
+    scale,
+    boxShadow:
+      "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+  }}
+  className="max-w-5xl mx-auto w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+>
+  <div className="w-full overflow-hidden md:rounded-2xl md:p-4 hidden sm:block">
+    {children}
+  </div>
+</motion.div>
+
   );
 };
