f76cf291336bca222fdb2d1af428400fd407f9b5
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 17:58:50 +0530

    Update Beam.jsx

--- src/components/Beam.jsx
+++ src/components/Beam.jsx
@@ -7,16 +7,18 @@ export function BackgroundBeamsWithCollisionDemo() {
     <BackgroundBeamsWithCollision>
       <div className="flex flex-col items-center justify-center h-screen text-center overflow-hidden relative z-10">
         {/* Heading */}
-        <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-white dark:text-black font-sans tracking-tight">
+        <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center font-sans tracking-tight">
           What&apos;s cooler than{" "}
           <img src={googleIcon} alt="Google" className="inline-block" />{" "}
           Calendar?{" "}
           <br />
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)] dark:text-pink-500">
+            {/* Gradient text for light mode */}
+            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)] dark:text-transparent dark:bg-gradient-to-r dark:from-yellow-400 dark:via-red-500 dark:to-pink-600">
               <span>Eventify.</span>
             </div>
-            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+            {/* Gradient text for dark mode */}
+            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4 dark:bg-gradient-to-r dark:from-yellow-400 dark:via-red-500 dark:to-pink-600">
               <span>Eventify.</span>
             </div>
           </div>
