4fc29d84f24c57c4cdf1d285ab6a16d2a1399c72
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 19:57:57 +0530

    Update 3.jsx

--- src/components/cards/3.jsx
+++ src/components/cards/3.jsx
@@ -31,25 +31,6 @@ const UseExistingToolsPage = () => {
         officia atque iure voluptatibus necessitatibus odit cupiditate
         reprehenderit iusto quaerat!
       </p>
-
-      {/* Button */}
-      <button className="px-6 py-3 bg-pink-500 text-white rounded-md hover:bg-pink-600 transition flex items-center">
-        LEARN MORE
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 ml-2"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17 8l4 4m0 0l-4 4m4-4H3"
-          />
-        </svg>
-      </button>
     </div>
   );
 };
