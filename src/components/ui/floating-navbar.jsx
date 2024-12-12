045716e82b746d40c024852da863cce104b14aea
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 16:08:17 +0530

    Update floating-navbar.jsx

--- src/components/ui/floating-navbar.jsx
+++ src/components/ui/floating-navbar.jsx
@@ -34,7 +34,7 @@ export const FloatingNav = ({ navItems, className }) => {
         }}
         transition={{ duration: 0.2 }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-white dark:border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-gray-500 dark:border-gray-700 rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
           className
         )}
       >
@@ -51,7 +51,7 @@ export const FloatingNav = ({ navItems, className }) => {
           </a>
         ))}
         <button
-          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-white px-4 py-2 rounded-full"
+          className="border text-sm font-medium relative border-neutral-200 dark:border-gray-700 text-white px-4 py-2 rounded-full"
         >
           <span>Login</span>
           <span
