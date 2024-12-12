0adb7d2c552df47b5f8aec83a5dc1bc4390413d4
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 14:39:09 +0530

    remove dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // Enable dark mode via 'class' strategy
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
