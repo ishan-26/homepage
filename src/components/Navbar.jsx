1f2d0b2632534439c752ab4e5ab80af0aa75d078
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-13 15:50:04 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import { FloatingNav } from "../components/ui/floating-navbar";
-import { IconHome, IconBrandSlack, IconCalendar, IconMessage } from "@tabler/icons-react"; // Add the missing icon import
+import { IconHome, IconBrandSlack} from "@tabler/icons-react";
 
 export function FloatingNavDemo() {
   const navItems = [
     {
       name: "Home",
-      link: "#home", // Set this to the correct route
+      link: "#home",
       icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Features",
       link: "#lamp",
-      icon: <Iconlayers-intersect className="h-4 w-4 text-neutral-500 dark:text-white" />, // Ensure IconBrandSlack is imported if used
+      icon: <IconBrandSlack className="h-4 w-4 text-neutral-500 dark:text-white" />, 
     },
-       {
-      name: "Info",
-      link: "#beam",
-      icon: <IconBrandSlack className="h-4 w-4 text-neutral-500 dark:text-white" />, // Ensure IconBrandSlack is imported if used
-    },
-  
-   
   ];
 
   return (
